8cef04764d92ae38acc7fd490e83167b23695042
RicoLiu <2869183609@qq.com>
2018-08-30 16:52:00 +0800

    add counter example

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapGetters, mapActions } from 'vuex'
+
+export default Vue.extend({
+    computed: mapGetters([
+      'evenOrOdd'
+    ]),
+    methods: mapActions([
+      'increment',
+      'decrement',
+      'incrementIfOdd',
+      'incrementAsync'
+    ])
+})
+</script>
+
